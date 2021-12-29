29b8ff0cb33fed130cd04b48a1f1556ec0139faf
paulojfigueira <paulojfigueira@yahoo.com.br>
2021-12-29 18:42:31 -0300

    Novas aulas Javascript

---
+++ ex03/script.js
@@ -0,0 +1,7 @@
+var nome = 'Bruno Otávio'
+var idade = 10
+var curso = 'Javascript'
+var nota = 9.5
+/*window.alert('O aluno  ' + nome + '  idade ' + idade + ' anos tirou a nota ' + nota + ' no curso de ' + curso)*/
+
+window.alert(`O aluno ${nome} com idade ${idade} tirou a nota ${nota} no curso de ${curso}`)
---
+++ ex04/script.js
@@ -0,0 +1,2 @@
+var nome = window.prompt('Digite seu nome: ')
+document.write(`Olá, ${nome.toUpperCase()}! Seu nome tem ${nome.length} letras. Nomes nunca são em minúsculas, desta forma: ${nome.toLowerCase()}.`)
