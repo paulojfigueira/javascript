9748a2f36e6dd5a7c9c93180495caabfa7f40388
paulojfigueira <paulojfigueira@yahoo.com.br>
2021-12-30 09:35:00 -0300

    Atualização da aula de JavaScript

---
+++ ex05/script.js
@@ -0,0 +1,12 @@
+/*Selecionando elementos DOM */
+
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[1]
+/*corpo.style.background = 'black'*/
+/*p1.style.color = 'blue'*/
+/*window.alert(p1.innerText)*/
+/*var d = window.document.getElementById('msg')
+d.style.background = 'green'*/
+/*d.innerText = 'Estou aguardando...'*/
+var d = window.document.querySelector('div#msg')
+d.style.background = 'black'
